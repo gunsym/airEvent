37c664fe229fe0be8408735f4fcb201bf3ddd4d0
Symphegy Main Tower <gun@symphegy.com.au>
2020-08-12 14:13:28 +1000

    init project

---
+++ android/app/src/main/kotlin/io/airevent/air_event/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.airevent.air_event
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+import 'package:air_event/app.dart';
+import 'package:air_event/simple_bloc_observer.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  EquatableConfig.stringify = kDebugMode;
+  Bloc.observer = SimpleBlocObserver();
+  runApp(App(authenticationRepository: AuthenticationRepository()));
+}
