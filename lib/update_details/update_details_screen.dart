86079487ddf9cf59ebf85c1260f3185bc7d83105
gunsym <gun@symphegy.com.au>
2020-09-14 12:23:57 +1000

    Revert "Add list for members"
    This reverts commit 939c02c716e95aeae2aae660fc474de58e3719ef.

--- lib/update_details/update_details_screen.dart
+++ lib/update_details/update_details_screen.dart
@@ -1,7 +1,5 @@
 import 'dart:async';
 import 'dart:convert';
-import 'package:air_event/update_details/bloc/list_bloc.dart';
-import 'package:air_event/update_details/models/models.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_form_bloc/flutter_form_bloc.dart';
@@ -234,42 +232,6 @@ class UpdateDetailsScreen extends StatelessWidget {
                                     prefixIcon: Icon(Icons.sentiment_satisfied),
                                   ),
                                 ),
-                                BlocBuilder<ListBloc, ListState>(
-                                  builder: (context, state) {
-                                    if (state is Failure) {
-                                      return Center(
-                                        child:
-                                            Text('Oops something went wrong!'),
-                                      );
-                                    }
-                                    if (state is Loaded) {
-                                      if (state.items.isEmpty) {
-                                        return Center(
-                                          child: Text('no content'),
-                                        );
-                                      }
-                                      return ListView.builder(
-                                        shrinkWrap: true,
-                                        physics:
-                                            const NeverScrollableScrollPhysics(),
-                                        itemBuilder:
-                                            (BuildContext context, int index) {
-                                          return ItemTile(
-                                            item: state.items[index],
-                                            onDeletePressed: (id) {
-                                              BlocProvider.of<ListBloc>(context)
-                                                  .add(Delete(id: id));
-                                            },
-                                          );
-                                        },
-                                        itemCount: state.items.length,
-                                      );
-                                    }
-                                    return Center(
-                                      child: CircularProgressIndicator(),
-                                    );
-                                  },
-                                ),
                                 BlocBuilder<ListFieldBloc<MemberFieldBloc>,
                                     ListFieldBlocState<MemberFieldBloc>>(
                                   cubit: formBloc.members,
@@ -316,31 +278,6 @@ class UpdateDetailsScreen extends StatelessWidget {
           );
         },
       ),
-    );
-  }
-}
-
-class ItemTile extends StatelessWidget {
-  final Item item;
-  final Function(String) onDeletePressed;
-
-  const ItemTile({
-    Key key,
-    @required this.item,
-    @required this.onDeletePressed,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      leading: Text('#${item.id}'),
-      title: Text(item.value),
-      trailing: item.isDeleting
-          ? CircularProgressIndicator()
-          : IconButton(
-              icon: Icon(Icons.delete, color: Colors.red),
-              onPressed: () => onDeletePressed(item.id),
-            ),
     );
   }
 }
