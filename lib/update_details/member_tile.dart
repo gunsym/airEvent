9451affaa9e8bbb3a050ebc6c3a57ddca0f28c15
Symphegy Main Tower <gun@symphegy.com.au>
2020-10-20 14:57:28 +1100

    Change edit member ux - wip

---
+++ lib/update_details/member_tile.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:members_repository/members_repository.dart';
+
+class MemberTile extends StatelessWidget {
+  final Member member;
+  final GestureTapCallback onTap;
+
+  MemberTile({
+    Key key,
+    @required this.member,
+    @required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
