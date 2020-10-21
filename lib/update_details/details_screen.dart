a6bbb507dab6ee4979c66c957d91d953d59fb485
gunsym <gun@symphegy.com.au>
2020-10-21 18:18:07 +1100

    Add form bloc to details screen - wip

--- lib/update_details/details_screen.dart
+++ lib/update_details/details_screen.dart
@@ -1,6 +1,8 @@
 import 'package:air_event/update_details/bloc/list_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_form_bloc/flutter_form_bloc.dart';
+import 'package:members_repository/members_repository.dart';
 
 class DetailsScreen extends StatelessWidget {
   final int index;
@@ -14,46 +16,222 @@ class DetailsScreen extends StatelessWidget {
         final members = (state as Loaded).members;
         final member = members.elementAt(index);
         return Scaffold(
-          appBar: AppBar(
-            title: Text('Member Details'),
-          ),
-          body: member == null
-              ? Container()
-              : Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: ListView(
-                    children: [
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Hero(
-                                  tag: '${member.id}__heroTag',
-                                  child: Container(
-                                    width: MediaQuery.of(context).size.width,
-                                    padding: EdgeInsets.only(
-                                      top: 8.0,
-                                      bottom: 16.0,
-                                    ),
-                                    child: Text(member.firstName),
-                                  ),
-                                ),
-                                Text(member.lastName),
-                                Text(member.email),
-                                Text(member.specialNeeds.toString()),
-                              ],
-                            ),
-                          )
-                        ],
-                      )
-                    ],
+            appBar: AppBar(
+              title: Text('Member Details'),
+            ),
+            body: member == null
+                ? Container()
+                : AllFieldsForm(
+                    member: member,
+                  )
+            // Padding(
+            //         padding: EdgeInsets.all(16.0),
+            //         child: ListView(
+            //           children: [
+            //             Row(
+            //               crossAxisAlignment: CrossAxisAlignment.start,
+            //               children: [
+            //                 Expanded(
+            //                   child: Column(
+            //                     crossAxisAlignment: CrossAxisAlignment.start,
+            //                     children: [
+            //                       Hero(
+            //                         tag: '${member.id}__heroTag',
+            //                         child: Container(
+            //                           width: MediaQuery.of(context).size.width,
+            //                           padding: EdgeInsets.only(
+            //                             top: 8.0,
+            //                             bottom: 16.0,
+            //                           ),
+            //                           child: Text(member.firstName),
+            //                         ),
+            //                       ),
+            //                       Text(member.lastName),
+            //                       Text(member.email),
+            //                       Text(member.specialNeeds.toString()),
+            //                     ],
+            //                   ),
+            //                 )
+            //               ],
+            //             )
+            //           ],
+            //         ),
+            //       ),
+            );
+      },
+    );
+  }
+}
+
+class AllFieldsFormBloc extends FormBloc<String, String> {
+  final firstName = TextFieldBloc(validators: [FieldBlocValidators.required]);
+  final lastName = TextFieldBloc(validators: [FieldBlocValidators.required]);
+  final email = TextFieldBloc(
+      validators: [FieldBlocValidators.required, FieldBlocValidators.email]);
+  final Member member;
+
+  AllFieldsFormBloc({@required this.member}) : super(isLoading: true) {
+    addFieldBlocs(fieldBlocs: [
+      firstName,
+      lastName,
+      email,
+    ]);
+  }
+
+  @override
+  void onLoading() async {
+    firstName.updateInitialValue(member.firstName);
+    lastName.updateInitialValue(member.lastName);
+    email.updateInitialValue(member.email);
+  }
+
+  @override
+  void onSubmitting() async {
+    try {
+      await Future<void>.delayed(Duration(milliseconds: 500));
+
+      emitSuccess(canSubmitAgain: true);
+    } catch (e) {
+      emitFailure();
+    }
+  }
+}
+
+class AllFieldsForm extends StatelessWidget {
+  final Member member;
+
+  AllFieldsForm({@required this.member});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => AllFieldsFormBloc(member: member),
+      child: Builder(
+        builder: (context) {
+          final formBloc = BlocProvider.of<AllFieldsFormBloc>(context);
+
+          return Theme(
+            data: Theme.of(context).copyWith(
+              inputDecorationTheme: InputDecorationTheme(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+            ),
+            child: Scaffold(
+              body: FormBlocListener<AllFieldsFormBloc, String, String>(
+                onSubmitting: (context, state) {
+                  LoadingDialog.show(context);
+                },
+                onSuccess: (context, state) {
+                  LoadingDialog.hide(context);
+
+                  Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (_) => SuccessScreen()));
+                },
+                onFailure: (context, state) {
+                  LoadingDialog.hide(context);
+
+                  Scaffold.of(context).showSnackBar(
+                      SnackBar(content: Text(state.failureResponse)));
+                },
+                child: SingleChildScrollView(
+                  physics: ClampingScrollPhysics(),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: <Widget>[
+                        TextFieldBlocBuilder(
+                          textFieldBloc: formBloc.firstName,
+                          decoration: InputDecoration(
+                            labelText: 'First Name',
+                            prefixIcon: Icon(Icons.text_fields),
+                          ),
+                        ),
+                        TextFieldBlocBuilder(
+                          textFieldBloc: formBloc.lastName,
+                          decoration: InputDecoration(
+                            labelText: 'Last Name',
+                            prefixIcon: Icon(Icons.text_fields),
+                          ),
+                        ),
+                        TextFieldBlocBuilder(
+                          textFieldBloc: formBloc.email,
+                          decoration: InputDecoration(
+                            labelText: 'Email',
+                            prefixIcon: Icon(Icons.email),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-        );
-      },
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class LoadingDialog extends StatelessWidget {
+  static void show(BuildContext context, {Key key}) => showDialog<void>(
+        context: context,
+        useRootNavigator: false,
+        barrierDismissible: false,
+        builder: (_) => LoadingDialog(key: key),
+      ).then((_) => FocusScope.of(context).requestFocus(FocusNode()));
+
+  static void hide(BuildContext context) => Navigator.pop(context);
+
+  LoadingDialog({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Center(
+        child: Card(
+          child: Container(
+            width: 80,
+            height: 80,
+            padding: EdgeInsets.all(12.0),
+            child: CircularProgressIndicator(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SuccessScreen extends StatelessWidget {
+  SuccessScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(Icons.tag_faces, size: 100),
+            SizedBox(height: 10),
+            Text(
+              'Success',
+              style: TextStyle(fontSize: 54, color: Colors.black),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 10),
+            RaisedButton.icon(
+              onPressed: () => Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (_) => AllFieldsForm())),
+              icon: Icon(Icons.replay),
+              label: Text('AGAIN'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
