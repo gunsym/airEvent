16a10bc0c16e2239d1735adb7bfe35508c78401e
gunsym <gun@symphegy.com.au>
2020-11-12 17:56:25 +1100

    Add events - wip

--- lib/event/event_event.dart
+++ lib/event/event_event.dart
@@ -9,39 +9,6 @@ abstract class EventEvent extends Equatable {
 
 class Fetch extends EventEvent {}
 
-class Delete extends EventEvent {
-  //final String id;
-  final int event;
-  final List<Event> events;
-
-  //const Deleted({@required this.id});
-  const Delete({@required this.event, this.events});
-
-  @override
-  List<Object> get props => [event, events];
-  //List<Object> get props => [id];
-
-  @override
-  String toString() => 'Delete { event: $event, events: $events }';
-//String toString() => 'Delete { id: $id }';
-}
-
-class Deleted extends EventEvent {
-  //final String id;
-  final int event;
-
-  //const Deleted({@required this.id});
-  const Deleted({@required this.event});
-
-  @override
-  List<Object> get props => [event];
-  //List<Object> get props => [id];
-
-  @override
-  String toString() => 'Deleted { event: $event }';
-//String toString() => 'Deleted { id: $id }';
-}
-
 class Updated extends EventEvent {
   final List<Event> events;
 
--- lib/event/event_page.dart
+++ lib/event/event_page.dart
@@ -11,8 +11,69 @@ class EventPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => EventBloc(membersRepository: membersRepository),
-      child: ,
+      create: (context) =>
+          EventBloc(membersRepository: membersRepository)..add(Fetch()),
+      child: Builder(
+        builder: (context) {
+          final bottom = MediaQuery.of(context).viewInsets.bottom;
+
+          return Theme(
+            data: Theme.of(context).copyWith(
+              inputDecorationTheme: InputDecorationTheme(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+            ),
+            child: Scaffold(
+              resizeToAvoidBottomPadding: false,
+              resizeToAvoidBottomInset: false,
+              appBar: AppBar(title: Text('Events')),
+              body: SingleChildScrollView(
+                reverse: true,
+                physics: ClampingScrollPhysics(),
+                child: Padding(
+                  padding: EdgeInsets.only(bottom: bottom),
+                  child: Column(
+                    children: <Widget>[
+                      BlocBuilder<EventBloc, EventState>(
+                        builder: (context, state) {
+                          print(state);
+                          if (state is Failure) {
+                            return Center(
+                              child: Text('Oops something went wrong!'),
+                            );
+                          }
+                          if (state is Loaded) {
+                            if (state.events.isEmpty) {
+                              return Center(
+                                child: Text('no content'),
+                              );
+                            }
+                            final events = state.events;
+                            return ListView.builder(
+                              shrinkWrap: true,
+                              physics: const NeverScrollableScrollPhysics(),
+                              itemBuilder: (BuildContext context, int index) {
+                                final event = events[index];
+                                return Text(event.name);
+                              },
+                              itemCount: state.events.length,
+                            );
+                          }
+                          return Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
